7125fe0a33dc5bf5491a9f83060d6e50febf6b11
craftware <craftware@929de70a-2c34-db14-2004-516d2e5ed2b6>
2011-01-23 10:41:19 +0000

    行番号消す 整理

--- trunk/c/actor.c
+++ trunk/c/actor.c
@@ -1,54 +1,56 @@
-10 #include "util.c"
-20
-30 struct actor{
-40  char alive;
-50  int x;
-60  int y;
-70  int _x;
-80  int _y;
-90  int xx;
-100  int yy;
-110  char * img;
-120  void (*update)(struct actor * self);
-130 };
-140
-150 void actor_create(struct actor * self){
-160  self->alive=1;
-170  self->x=0;
-180  self->y=0;
-190  self->_x=0;
-200  self->_y=0;
-210  self->xx=0;
-220  self->yy=0;
-230  self->img=0;
-240  self->update=0;
-250 }
-260 void actor_update(struct actor * a, int num){
-270  int i;
-280  for(i=0;i<num;i++){
-290   a[i]._x=a[i].x;
-300   a[i]._y=a[i].y;
-310   a[i].x+=a[i].xx;
-320   a[i].y+=a[i].yy;
-330   if(a[i].update){a[i].update(&a[i]);}
-340  }
-350 }
-360
-370 void actor_draw(struct actor * a, int num){
-380  int i;
-390  for(i=0;i<num;i++){
-400   gcursor(a[i]._x>>4,a[i]._y>>4);
-410   gprint("0000000000000000");
-420   gcursor(a[i].x>>4,a[i].y>>4);
-430   gprint(a[i].img);
-440  }
-450 }
-460
-470 struct actor * actor_hittest(struct actor * a,struct actor * b,int num, int r){
-480  int i;
-490  for(i=0;i<num;i++){
-500   if(abs(a->x-b[i].x)<r && abs(a->y-b[i].y)<r ) return &b[i];
-510  }
-520 return 0;
-530 }
+
+struct actor{
+ char alive;
+ int x;
+ int y;
+ int _x;
+ int _y;
+ int xx;
+ int yy;
+ char * img;
+ void (*update)(struct actor * self);
+};
+
+void actor_create(struct actor * self){
+ self->alive=1;
+ self->x=0;
+ self->y=0;
+ self->_x=0;
+ self->_y=0;
+ self->xx=0;
+ self->yy=0;
+ self->img=0;
+ self->update=0;
+}
+void actor_update(struct actor * a, int num){
+ int i;
+ for(i=0;i<num;i++){
+  if(a[i].alive==0) continue;
+  a[i]._x=a[i].x;
+  a[i]._y=a[i].y;
+  a[i].x+=a[i].xx;
+  a[i].y+=a[i].yy;
+  if(a[i].update){a[i].update(&a[i]);}
+ }
+}
+
+void actor_draw(struct actor * a, int num){
+ int i;
+ for(i=0;i<num;i++){
+  if(a[i].alive==0) continue;
+  gcursor(a[i]._x>>4,a[i]._y>>4);
+  gprint("0000000000000000");
+  gcursor(a[i].x>>4,a[i].y>>4);
+  gprint(a[i].img);
+ }
+}
+
+struct actor * actor_hittest(struct actor * a,struct actor * b,int num, int r){
+ int i;
+ if(a->alive==0) return 0;
+ for(i=0;i<num;i++){
+  if(b[i].alive && abs(a->x-b[i].x)<r && abs(a->y-b[i].y)<r ) return &b[i];
+ }
+ return 0;
+}
 
